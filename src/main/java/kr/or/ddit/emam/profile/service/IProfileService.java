10b200131fe8c964c6e3f7a8956229e857b86570
seoyoham <syh7270@gmail.com>
2025-03-05 16:58:16 +0900

    서요한 - 프로필 화면 작성 *추가 작업 예정(프로필 수정, 게시글 등록 등)

---
+++ src/main/java/kr/or/ddit/emam/profile/service/IProfileService.java
@@ -0,0 +1,25 @@
+package kr.or.ddit.emam.profile.service;
+
+import java.util.List;
+
+import kr.or.ddit.emam.vo.PostVO;
+import kr.or.ddit.emam.vo.ProfileVO;
+
+public interface IProfileService {
+
+    /**
+     * 회원 ID를 이용하여 프로필 정보 (ProfileVO) 를 조회하는 메소드
+     * @param memId 조회할 회원 ID
+     * @return ProfileVO 프로필 정보, 정보가 없을 경우 null 반환
+     */
+    public ProfileVO getProfile(String memId);
+
+    /**
+     * 회원 ID를 이용하여 게시글 목록 (List<PostVO>) 을 최신순으로 조회하는 메소드
+     * @param memId 조회할 회원 ID
+     * @return List<PostVO> 게시글 목록, 게시글이 없을 경우 빈 List 반환
+     */
+    public List<PostVO> getPostList(String memId);
+
+
+}
