bac589d8c4a98b46cf3ccb00e36289457b7fa208
jewoo827 <jewoo827@naver.com>
2025-03-06 12:38:39 +0900

    우정은 - 알림기능 구현

---
+++ src/main/java/kr/or/ddit/emam/notification/controller/NotificationList.java
@@ -0,0 +1,50 @@
+package kr.or.ddit.emam.notification.controller;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import kr.or.ddit.emam.member.service.IMemberService;
+import kr.or.ddit.emam.member.service.MemberServiceImpl;
+import kr.or.ddit.emam.notification.service.INotificationService;
+import kr.or.ddit.emam.notification.service.NotificationServiceImpl;
+import kr.or.ddit.emam.vo.MemberVO;
+import kr.or.ddit.emam.vo.NotificationVO;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/notification/notificationList.do")
+public class NotificationList extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        //로그인한 계정정보 가져오기
+        HttpSession session = request.getSession();
+        MemberVO loginMemberVo = (MemberVO) session.getAttribute("loginMember");
+
+        //service객체 구하기
+        IMemberService memberService = MemberServiceImpl.getInstance();
+        INotificationService notificationService = NotificationServiceImpl.getInstance();
+
+        String mem_id = loginMemberVo.getMem_id();
+        List<NotificationVO> notificationList = notificationService.selectNotification(mem_id);
+        request.setAttribute("notificationList", notificationList);
+
+        int totalNotification = notificationService.totalCount(mem_id);
+        request.setAttribute("totalNotification", totalNotification);
+
+        request.getRequestDispatcher("").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
---
+++ src/main/java/kr/or/ddit/emam/notification/service/NotificationServiceImpl.java
@@ -0,0 +1,44 @@
+package kr.or.ddit.emam.notification.service;
+
+import kr.or.ddit.emam.notification.dao.INotificationDao;
+import kr.or.ddit.emam.notification.dao.NotificationDaoImpl;
+import kr.or.ddit.emam.vo.NotificationVO;
+
+import java.util.List;
+
+public class NotificationServiceImpl implements INotificationService {
+
+    //dao객체
+    private INotificationDao dao;
+    //자신의 객체
+    private static INotificationService service;
+
+    //생성자 - dao객체 열기
+    private NotificationServiceImpl() { dao = NotificationDaoImpl.getInstance(); }
+
+    //자신의 객체를 생성하고 리턴하는 메소드
+    public static INotificationService getInstance() {
+        if (service == null) service = new NotificationServiceImpl();
+        return service;
+    }
+
+    //알림 리스트
+    @Override
+    public List<NotificationVO> selectNotification(String mem_id) { return dao.selectNotification(mem_id); }
+
+    //미확인 알림 갯수 구하기
+    @Override
+    public int totalCount(String mem_id) { return dao.totalCount(mem_id); }
+
+    //알림 발생
+    @Override
+    public int insertNotification(NotificationVO notificationVo) { return dao.insertNotification(notificationVo); }
+
+    //알림 삭제
+    @Override
+    public int deleteNotification(int num) { return dao.deleteNotification(num); }
+
+    //알림 확인
+    @Override
+    public int updateNotification(int num) { return dao.updateNotification(num); }
+}
