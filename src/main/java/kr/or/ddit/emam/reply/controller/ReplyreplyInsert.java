330f51c05ff488a868cc9735379130342f6706c6
ddit <qndlrndl2@gmail.com>
2025-03-10 15:38:34 +0900

    노병훈 -> 댓글 수정

--- src/main/java/kr/or/ddit/emam/reply/controller/ReplyreplyInsert.java
+++ src/main/java/kr/or/ddit/emam/reply/controller/ReplyreplyInsert.java
@@ -6,8 +6,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.or.ddit.emam.profile.service.IProfileService;
+import kr.or.ddit.emam.profile.service.ProfileServiceImpl;
 import kr.or.ddit.emam.reply.service.IReplyService;
 import kr.or.ddit.emam.reply.service.ReplyServiceImpl;
+import kr.or.ddit.emam.vo.ProfileVO;
 import kr.or.ddit.emam.vo.ReplyVO;
 
 import java.io.IOException;
@@ -23,6 +26,7 @@ public class ReplyreplyInsert extends HttpServlet {
 
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html");
+        IProfileService profileService = ProfileServiceImpl.getInstance();
         //댓글 객체 생성
         IReplyService replyService = ReplyServiceImpl.getInstance();
 
@@ -53,7 +57,10 @@ public class ReplyreplyInsert extends HttpServlet {
             ReplyVO replyVO2 = new ReplyVO(replyindexInt,postindexInt);
 
             List<ReplyVO> ReplyList = replyService.selectReplyReplyList(replyVO2);
-
+            for(ReplyVO replyVO3 : ReplyList){
+                ProfileVO pfVO = profileService.selectProfile(replyVO3.getMem_id());
+                replyVO3.setProfileVo(pfVO);
+            }
             Gson gson = new Gson();
             String jsonData = null; //변환된 Json문자열이 저장될 변수
 
