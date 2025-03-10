d5dbfdc08a3233692956a4dd0b6810ba68f19429
YooJ9 <vmfls789@naver.com>
2025-03-10 11:35:03 +0900

    강유정 - 파일 업로드 충돌 때문에 다시 커밋(원래 커밋된 경로로 수정 후 개인 경로 주석 처리)

--- src/main/java/kr/or/ddit/emam/post/service/PostPhotoServiceImpl.java
+++ src/main/java/kr/or/ddit/emam/post/service/PostPhotoServiceImpl.java
@@ -34,6 +34,7 @@ public class PostPhotoServiceImpl implements IPostPhotoService {
         String tttt = this.getClass().getResource("/").getPath();
         System.out.println("tttt: " +tttt);
 
+        // /Users/beryz/Desktop/emam/src/main/webapp/images;
 //        String uploadPath = "D:/emam/src/main/webapp/upload";
         File uploadDir =new File(uploadPath);
         if(!uploadDir.exists()) {
